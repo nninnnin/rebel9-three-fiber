8bb51c10eab4c5c9b296b6f30023d21d67cb074c
이동규(Justin) <34882520+nninnnin@users.noreply.github.com>
2023-08-16 14:39:04 +0900

    바닥(planeGeometry) 추가

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # 레벨나인 과제 (React three fiber)
+
+## Requirements
+
+- [x] Canvas에 바닥 색상을 칠합니다. (#AAAAAA)
+- [ ] Canvas에 6개의 Panel wall 을 세웁니다.
+- [ ] 6개의 Panel wall은 예제 링크와 동일하게 위치시킵니다.
+- [ ] 각각 Panel 에는 다음과 같은 내용이 들어가 있습니다.
+      a. Color: 색상
+      b. Image: 이미지 (jpg or png)
+      c. Video: 영상 (mp4) 혹은 유튜브 영상
+      (각 내용은 정해진것이 없으며 아무 색상, 파일을 넣어서 보여주면 됩니다.)
+- [ ] AmbientLight와 Spotlight 을 추가합니다. (특정한 값은 없으며 보기에 적절한 값을 지정합니다.)
+- [ ] Mousewheel 로 z 축을 이동할 수 있습니다. (예제 참고)
+- [ ] Rotate는 비활성화합니다.
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { createRoot } from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 
+import { MeshReflectorMaterial } from "@react-three/drei";
+
 function App() {
   return (
     <div id="canvas-container">
       <Canvas dpr={[1, 1.5]} camera={{ fov: 70, position: [0, 2, 15] }}>
-        <color attach="background" args={["#191920"]} />
-        <ambientLight intensity={0.1} />
-        <directionalLight color="red" position={[0, 0, 5]} />
+        {/* 배경 */}
+        <color attach="background" args={["#fff"]} />
 
-        <mesh>
-          <boxGeometry args={[2, 2, 2]} />
-          <meshStandardMaterial />
+        {/* 바닥 */}
+        <mesh rotation={[-Math.PI / 2, 0, 0]}>
+          <planeGeometry args={[50, 50]} />
+          <MeshReflectorMaterial mirror={0} color="#AAAAAA" />
         </mesh>
       </Canvas>
     </div>
